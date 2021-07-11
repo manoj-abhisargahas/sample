9e6a49a1f41fc7ec4da23285b87ca96e76836b1d
manoj <manojabhisargahas1@gmail.com>
2021-07-12 04:33:10 +0530

    Delete sql operations directory

--- sql operations/show_all_list_queries.sql
+++
@@ -1,7 +0,0 @@
-/* SELECT INFO: */
-/* ============ */ (
-SELECT * FROM vyaparalavadevi_schema.contacts;
-SELECT * FROM vyaparalavadevi_schema.metal_names;
-SELECT * FROM vyaparalavadevi_schema.metal_purity_percentages;
-SELECT * FROM vyaparalavadevi_schema.ornament_names;
-)
